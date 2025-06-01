64933181093ec152f22f4f7be1ed0b29cd6e6573
Cameron Jones <iamcrjones@gmail.com>
2025-06-01 20:13:58 +1000

    Neogit test

---
+++ lua/custom/plugins/git.lua
@@ -0,0 +1,36 @@
+return {
+  -- {
+  --   'kdheepak/lazygit.nvim',
+  --   lazy = true,
+  --   cmd = {
+  --     'LazyGit',
+  --     'LazyGitConfig',
+  --     'LazyGitCurrentFile',
+  --     'LazyGitFilter',
+  --     'LazyGitFilterCurrentFile',
+  --   },
+  --   -- optional for floating window border decoration
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --   },
+  -- },
+  {
+    'NeogitOrg/neogit',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      local neogit = require 'neogit'
+
+      neogit.setup {
+        kind = 'vsplit',
+        integrations = {
+          telescope = true,
+          diffview = true,
+        },
+      }
+    end,
+  },
+}
