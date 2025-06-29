6f1a01679b55504c346ffffa18f2ccdcef6625df
Cameron Jones <iamcrjones@gmail.com>
2025-06-29 16:03:20 +1000

    update

--- init.lua
+++ init.lua
@@ -696,7 +696,7 @@ require('lazy').setup({
         javascriptreact = { 'prettier', stop_after_first = true },
         typescript = { 'prettier', stop_after_first = true },
         typescriptreact = { 'prettier', stop_after_first = true },
-        php = { 'php-cs-fixer' },
+        php = { 'pint' },
       },
     },
   },
@@ -723,12 +723,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
