5fa3ef4b7b5c80f799f664360de131e9c021c3a9
Cameron Jones <cameron.jones@rexsoftware.com.au>
2025-08-28 09:51:06 +1000

    add blame check

--- init.lua
+++ init.lua
@@ -115,6 +115,7 @@ vim.keymap.set('n', '<S-l>', ':bnext<cr>', { desc = 'Next Buffer' })
 vim.keymap.set('n', '<leader>bd', ':bd<cr>', { desc = 'Delete Buffer' })
 vim.keymap.set('n', '<leader>bo', ':%bd|edit#|bd#<cr>', { desc = 'Delete Other Buffers' })
 vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<cr>', { desc = 'Open Neogit' })
+vim.keymap.set('n', '<leader>gb', '<cmd>Gitsigns blame<cr>', { desc = 'View git blame of current buffer' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
