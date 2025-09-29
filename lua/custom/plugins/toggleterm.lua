5cc10c3f5e28b0d4a8c7bd5a210c0494cf4cf019
Cameron Jones <iamcrjones@gmail.com>
2025-09-29 22:00:38 +1000

    update bind

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
     require('toggleterm').setup {
       -- Configuration options (optional)
       -- size = 20,
-      open_mapping = [[<C-Enter>]],
+      open_mapping = [[<C-_>]],
       direction = 'float',
       float_opts = {
         border = 'curved',
